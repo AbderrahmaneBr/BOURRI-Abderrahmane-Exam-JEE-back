19d857f7bf64a012f36fa08c5aaced88b55cb0d9
Abderrahmane Bourri <abderrahmanebourri03@gmail.com>
2025-05-19 11:30:27 +0100

    Change FindMethod user details

--- src/main/java/org/example/examback/services/impl/CustomUserDetailsService.java
+++ src/main/java/org/example/examback/services/impl/CustomUserDetailsService.java
@@ -22,7 +22,7 @@ public class CustomUserDetailsService implements UserDetailsService {
     @Override
     public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
         // Example with a User entity
-        User user = userRepository.findByUsername(username)
+        User user = userRepository.findByEmail(username)
                 .orElseThrow(() -> new UsernameNotFoundException("User not found"));
 
         return new org.springframework.security.core.userdetails.User(
